52263b977cecaa7fdbc8b589da25aab6a40843cb
jacek swoboda <jacswob@gmail.com>
2020-03-02 21:00:57 +0100

    Using index route

--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 export default class Router extends EmberRouter {
   location = config.locationType;
@@ -7,5 +7,9 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-  this.route('route-name');
+  this.route("route-name");
+  this.route("posts", function() {
+    this.route("index", { path: "/" });
+    this.route("post", { path: "/:post_id" });
+  });
 });
