dfaf73d8a263c1fa86219bd4318961b58a6a24d5
jacek swoboda <jacswob@gmail.com>
2020-03-03 15:48:10 +0100

    Change router template name

--- app/routes/route-name.js
+++ app/routes/route-name.js
@@ -3,6 +3,7 @@ import RSVP from "rsvp";
 import { A } from "@ember/array";
 
 export default class RouteNameRoute extends Route {
+  templateName = "route-namee";
   model() {
     return RSVP.hash({
       songs: A([{ name: "Bury me low" }, { name: "Call my name" }]),
